b4411675cd8382d8b02f01f87e4030bafb8af494
Eric Bello <ebello219@gmail.com>
2023-09-15 13:48:59 -0400

    added static file export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    output: 'export',
+    // exportPathMap: async function (defaultPathMap) {
+    //   return {
+    //     '/': { page: '/' },
+    //     '/about': { page: '/about' },
+    //     '/blog': { page: '/blog' },
+    //     '/contact': { page: '/contact' },
+    //     '/experience': { page: '/experience' },
+    //     '/projects': { page: '/projects' },
+    //   };
+    // },
+    // trailingSlash: true,
+    // images: {
+    //   loader: 'akamai',
+    //   path: '',
+    // },
+  }
