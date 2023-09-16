60333d052ed596dd3a93e9c20f00f44bea2400ff
Eric Bello <ebello219@gmail.com>
2023-09-16 10:53:22 -0400

    fixed Malformed PostCSS Configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   plugins: [
-      require('postcss-import'),
-      require('tailwindcss/nesting')(require('postcss-nesting')),
-      require('autoprefixer'),
-      require('tailwindcss'),
+      'postcss-import',
+      'tailwindcss/nesting',
+      'postcss-nesting',
+      'autoprefixer',
+      'tailwindcss',
     ],
 }
