7adb6bd3a507d75763795e589a11529f01ccd0c3
Eric Bello <ebello219@gmail.com>
2023-09-19 22:52:05 -0400

    updated page content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import '../styles/styles.css';
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from "next/script";
 import { Inter } from 'next/font/google'
-
 const inter = Inter({ subsets: ['latin'] })
+import '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/free-brands-svg-icons';
+import  '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/react-fontawesome';
 
 export const metadata: Metadata = {
   title: 'Eric Bello Cloud Resume',
@@ -16,7 +21,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      </body>
     </html>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/styles.css'
  
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <html>
+      <Head>
+        <script src="https://kit.fontawesome.com/a4f5122e74.js" crossOrigin="anonymous"></script>
+      </Head>
+      <Component {...pageProps} />
+    </html>
+  )
 }
