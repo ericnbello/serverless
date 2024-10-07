bce01bf34a08ac08e5aa67c73290b34061df21db
Eric Bello <inviter.headset0n@icloud.com>
2024-10-06 20:18:23 -0400

    updated cors setting

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
     return {      
         'statusCode': 200,
         'headers': {
-            'Access-Control-Allow-Origin': 'www.cloud.ericnbello.com',
+            'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
             'Access-Control-Allow-Credentials': 'true',
             'Content-Type': 'application/json'
