da29f2b04e89eddba81b5e7378e4c58e34d78bef
Eric Bello <inviter.headset0n@icloud.com>
2024-10-08 14:53:10 -0400

    updated api endpoint

--- components/VisitorCounter.js
+++ components/VisitorCounter.js
@@ -10,7 +10,7 @@ export default function VisitorCounter() {
 
     useEffect(() => {
         async function getCount() {
-            const response = await fetch('https://uim0epv297.execute-api.us-east-1.amazonaws.com/Prod/counter');
+            const response = await fetch('https://cbq78s9fa0.execute-api.us-east-1.amazonaws.com/Prod/counter');
             const data = await response.json();
             console.log(data)
             setCount(data);
